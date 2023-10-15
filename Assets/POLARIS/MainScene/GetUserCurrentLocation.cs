02c6cb9f0f371983e428bd32c2e7a786abf78e1b
Kevin <74631846+Kooven47@users.noreply.github.com>
2023-10-14 21:06:20 -0400

    BIG ASS COMMIT CHECK DESCRIPTION FOR DEETS!!!!
    Converted suggestions and search results from labels to ListViews
    Made enter button search
    Convert search box text to real ass placeholder
    Clicking a building now fetches its name from the DB and gets its relevant info
    Added a script to get users current long and lat, works on android
    Remove find button
    Make routing display "point" instead of "Isgameobject" lol

---
+++ Assets/POLARIS/MainScene/GetUserCurrentLocation.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+using System.Collections;
+
+public class GetUserCurrentLocation : MonoBehaviour
+{
+    private float _latitude = 0f;
+    private float _longitude = 0f;
+    private void Start()
+    {
+        StartCoroutine(LocationCoroutine());
+    }
+
+    private void Update()
+    {
+        // if (_latitude != 0f) Debug.Log($"Latitude: {_latitude}");
+        // if (_longitude != 0f) Debug.Log($"Longitude: {_longitude}");
+    }
+    
+    IEnumerator LocationCoroutine() 
+    {
+    // Uncomment if you want to test with Unity Remote
+/*#if UNITY_EDITOR
+        yield return new WaitWhile(() => !UnityEditor.EditorApplication.isRemoteConnected);
+        yield return new WaitForSecondsRealtime(5f);
+#endif*/
+#if UNITY_EDITOR
+        // No permission handling needed in Editor
+#elif UNITY_ANDROID
+        if (!UnityEngine.Android.Permission.HasUserAuthorizedPermission(UnityEngine.Android.Permission.CoarseLocation)) {
+            UnityEngine.Android.Permission.RequestUserPermission(UnityEngine.Android.Permission.CoarseLocation);
+        }
+
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser) {
+            // TODO Failure
+            Debug.LogFormat("Android and Location not enabled");
+            yield break;
+        }
+
+#elif UNITY_IOS
+        if (!Input.location.isEnabledByUser) {
+            // TODO Failure
+            Debug.LogFormat("IOS and Location not enabled");
+            yield break;
+        }
+#endif
+        // Start service before querying location
+        Input.location.Start(500f, 500f);
+                
+        // Wait until service initializes
+        int maxWait = 15;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) {
+            yield return new WaitForSecondsRealtime(1);
+            maxWait--;
+        }
+
+        // Editor has a bug which doesn't set the service status to Initializing. So extra wait in Editor.
+#if UNITY_EDITOR
+        int editorMaxWait = 15;
+        while (Input.location.status == LocationServiceStatus.Stopped && editorMaxWait > 0) {
+            yield return new WaitForSecondsRealtime(1);
+            editorMaxWait--;
+        }
+#endif
+
+        // Service didn't initialize in 15 seconds
+        if (maxWait < 1) {
+            // TODO Failure
+            Debug.LogFormat("Timed out");
+            yield break;
+        }
+
+        // Connection has failed
+        if (Input.location.status != LocationServiceStatus.Running) 
+        {
+            // TODO Failure
+            Debug.LogFormat("Unable to determine device location. Failed with status {0}", Input.location.status);
+            yield break;
+        } 
+        else 
+        {
+            Debug.LogFormat("Location service live. status {0}", Input.location.status);
+            // Access granted and location value could be retrieved
+            Debug.LogFormat("Location: " 
+                + Input.location.lastData.latitude + " " 
+                + Input.location.lastData.longitude + " " 
+                + Input.location.lastData.altitude + " " 
+                + Input.location.lastData.horizontalAccuracy + " " 
+                + Input.location.lastData.timestamp);
+
+            _latitude = Input.location.lastData.latitude;
+            _longitude = Input.location.lastData.longitude;
+            // TODO success do something with location
+        }
+
+        // Stop service if there is no need to query location updates continuously
+        Input.location.Stop();
+    }
+}
