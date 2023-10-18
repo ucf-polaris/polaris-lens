1b14e91c9f591a075f24aae1f95c1cbe7aee75f5
Ethan Bliss <et505781@ucf.edu>
2023-10-18 15:43:10 -0400

    fix tabs and animate opening

--- Assets/POLARIS/ChangeTabImage.cs
+++ Assets/POLARIS/ChangeTabImage.cs
@@ -11,6 +11,10 @@ public class ChangeTabImage : MonoBehaviour
 	private Texture2D _calendar;
 	private Texture2D _calendarDark;
 
+	private VisualElement _spacer;
+	private string _lastPressed;
+	private bool _menuOpen;
+
 	// Start is called before the first frame update
     private void Start()
     {
@@ -22,23 +26,35 @@ public class ChangeTabImage : MonoBehaviour
 		_locationButton = uiDoc.rootVisualElement.Q<Button>("Loc");
 		_locationButton.clickable.clicked += OnClickLocation;
 		
-		_location = Resources.Load<Texture2D>("Polaris/UI tabs/location");
+		_location = Resources.Load<Texture2D>("Polaris/UI tabs/locationlight");
 		_locationDark = Resources.Load<Texture2D>("Polaris/UI tabs/locationdark");
-		_calendar = Resources.Load<Texture2D>("Polaris/UI tabs/calendar");
+		_calendar = Resources.Load<Texture2D>("Polaris/UI tabs/calendarlight");
 		_calendarDark = Resources.Load<Texture2D>("Polaris/UI tabs/calendardark");
+		
+		_spacer = uiDoc.rootVisualElement.Q<VisualElement>("Spacer");
     }
 
     private void OnClickCalendar()
-		{
-			_locationButton.style.backgroundImage = _locationDark;
-			_calendarButton.style.backgroundImage = _calendar;
-			print("Hello calendar!");
-		}
+	{
+		_locationButton.style.backgroundImage = _locationDark;
+		_calendarButton.style.backgroundImage = _calendar;
+		print("Hello calendar!");
+		MoveUI("calendar");
+	}
 
     private void OnClickLocation()
-		{
-			_locationButton.style.backgroundImage = _location;
-			_calendarButton.style.backgroundImage = _calendarDark;
-			print("Hello location!");
-		}
+	{
+		_locationButton.style.backgroundImage = _location;
+		_calendarButton.style.backgroundImage = _calendarDark;
+		print("Hello location!");
+		MoveUI("location");
+	}
+
+    private void MoveUI(string pressed)
+    {
+	    _menuOpen = !(_menuOpen && _lastPressed == pressed);
+	    _lastPressed = pressed;
+
+	    _spacer.style.height = Length.Percent(_menuOpen ?  15 : 80);
+    }
 }
